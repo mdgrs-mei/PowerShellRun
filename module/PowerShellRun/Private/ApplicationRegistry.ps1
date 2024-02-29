9040f1522ed376f722068e213e0109490141a949
Andrew Pla <AndrewPla@Outlook.com>
2024-02-29 00:00:22 -0500

    added -Contains

--- module/PowerShellRun/Private/ApplicationRegistry.ps1
+++ module/PowerShellRun/Private/ApplicationRegistry.ps1
@@ -1,30 +1,24 @@
-class ApplicationRegistry
-{
+class ApplicationRegistry {
     $sync = [System.Collections.Hashtable]::Synchronized(@{})
     $registerEntryJob = $null
 
-    [System.Collections.Generic.List[PowerShellRun.SelectorEntry]] GetEntries()
-    {
+    [System.Collections.Generic.List[PowerShellRun.SelectorEntry]] GetEntries() {
         return $this.sync.entries
     }
 
-    [void] EnableEntries([String[]]$categories)
-    {
+    [void] EnableEntries([String[]]$categories) {
         # Wait for the previous job if exists
         $this.UpdateEntries()
 
-        if ($script:isWindows)
-        {
+        if ($script:isWindows) {
             $this.StartRegisterEntriesWindows($categories)
         }
-        elseif ($script:isMacOs)
-        {
+        elseif ($script:isMacOs) {
             $this.StartRegisterEntriesMacOs($categories)
         }
     }
 
-    [void] StartRegisterEntriesWindows($categories)
-    {
+    [void] StartRegisterEntriesWindows($categories) {
         $actionKeys = @(
             [PowerShellRun.ActionKey]::new($script:globalStore.firstActionKey, 'Launch App')
             [PowerShellRun.ActionKey]::new($script:globalStore.secondActionKey, 'Launch App as Admin')
@@ -32,12 +26,10 @@ class ApplicationRegistry
         $callback = {
             $result = $args[0].Result
             $path = $args[0].ArgumentList
-            if ($result.KeyCombination -eq $script:globalStore.firstActionKey)
-            {
+            if ($result.KeyCombination -eq $script:globalStore.firstActionKey) {
                 & $script:globalStore.invokeFile $path
             }
-            elseif ($result.KeyCombination -eq $script:globalStore.secondActionKey)
-            {
+            elseif ($result.KeyCombination -eq $script:globalStore.secondActionKey) {
                 Start-Process $path -Verb runAs
             }
         }
@@ -47,8 +39,7 @@ class ApplicationRegistry
 
             $entries = [System.Collections.Generic.List[PowerShellRun.SelectorEntry]]::new()
 
-            if ($categories.Contains('Application'))
-            {
+            if ($categories -Contains 'Application') {
                 # Start Menu shortcuts
                 $startMenuFolders = @(
                     "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs",
@@ -57,11 +48,9 @@ class ApplicationRegistry
 
                 $shell = New-Object -ComObject WScript.Shell
 
-                foreach ($folder in $startMenuFolders)
-                {
+                foreach ($folder in $startMenuFolders) {
                     $links = Get-ChildItem $folder -Filter '*.lnk' -Recurse
-                    foreach ($link in $links)
-                    {
+                    foreach ($link in $links) {
                         $shortcut = $shell.CreateShortcut($link.FullName)
                         $entry = [PowerShellRun.SelectorEntry]::new()
                         $entry.Icon = '🚀'
@@ -71,7 +60,7 @@ class ApplicationRegistry
                         $entry.ActionKeys = $actionKeys
 
                         $entry.UserData = @{
-                            ScriptBlock = $callback
+                            ScriptBlock  = $callback
                             ArgumentList = $link.FullName
                         }
 
@@ -82,10 +71,8 @@ class ApplicationRegistry
                 # Store Apps
                 $shell = New-Object -ComObject shell.application
                 $appsFolder = $shell.NameSpace("shell:AppsFolder")
-                foreach ($item in $appsFolder.Items())
-                {
-                    if (-not $item.Path.Contains('!'))
-                    {
+                foreach ($item in $appsFolder.Items()) {
+                    if (-not $item.Path.Contains('!')) {
                         continue
                     }
                     $path = "shell:AppsFolder\{0}" -f $item.Path
@@ -97,7 +84,7 @@ class ApplicationRegistry
                     $entry.ActionKeys = $actionKeys[0]
 
                     $entry.UserData = @{
-                        ScriptBlock = $callback
+                        ScriptBlock  = $callback
                         ArgumentList = $path
                     }
 
@@ -106,11 +93,9 @@ class ApplicationRegistry
             }
 
             # Executables in Path
-            if ($categories.Contains('Executable'))
-            {
+            if ($categories -Contains 'Executable') {
                 $apps = Get-Command -CommandType Application
-                foreach ($app in $apps)
-                {
+                foreach ($app in $apps) {
                     $entry = [PowerShellRun.SelectorEntry]::new()
                     $entry.Icon = '🔧'
                     $entry.Name = $app.Name
@@ -118,7 +103,7 @@ class ApplicationRegistry
                     $entry.ActionKeys = $actionKeys
 
                     $entry.UserData = @{
-                        ScriptBlock = $callback
+                        ScriptBlock  = $callback
                         ArgumentList = $app.Source
                     }
 
@@ -130,16 +115,14 @@ class ApplicationRegistry
         } -ArgumentList $categories, $actionKeys, $callback, $this.sync
     }
 
-    [void] StartRegisterEntriesMacOs($categories)
-    {
+    [void] StartRegisterEntriesMacOs($categories) {
         $actionKeys = @(
             [PowerShellRun.ActionKey]::new($script:globalStore.firstActionKey, 'Launch App')
         )
         $callback = {
             $result = $args[0].Result
             $fullName = $args[0].ArgumentList
-            if ($result.KeyCombination -eq $script:globalStore.firstActionKey)
-            {
+            if ($result.KeyCombination -eq $script:globalStore.firstActionKey) {
                 & $script:globalStore.invokeFile $fullName
             }
         }
@@ -149,8 +132,7 @@ class ApplicationRegistry
 
             $entries = [System.Collections.Generic.List[PowerShellRun.SelectorEntry]]::new()
 
-            if ($categories.Contains('Application'))
-            {
+            if ($categories -Contains 'Application' ) {
                 $folders = @(
                     '/Applications'
                     '/Applications/Utilities'
@@ -159,11 +141,9 @@ class ApplicationRegistry
                     '/System/Library/CoreServices/Applications'
                 )
 
-                foreach ($folder in $folders)
-                {
+                foreach ($folder in $folders) {
                     $apps = Get-ChildItem $folder -Filter '*.app'
-                    foreach ($app in $apps)
-                    {
+                    foreach ($app in $apps) {
                         $entry = [PowerShellRun.SelectorEntry]::new()
                         $entry.Icon = '🚀'
                         $entry.Name = [System.IO.Path]::GetFileNameWithoutExtension($app.BaseName)
@@ -171,7 +151,7 @@ class ApplicationRegistry
                         $entry.ActionKeys = $actionKeys
 
                         $entry.UserData = @{
-                            ScriptBlock = $callback
+                            ScriptBlock  = $callback
                             ArgumentList = $app.FullName
                         }
 
@@ -181,11 +161,9 @@ class ApplicationRegistry
             }
 
             # Executables in Path
-            if ($categories.Contains('Executable'))
-            {
+            if ($categories -Contains 'Executable') {
                 $apps = Get-Command -CommandType Application
-                foreach ($app in $apps)
-                {
+                foreach ($app in $apps) {
                     $entry = [PowerShellRun.SelectorEntry]::new()
                     $entry.Icon = '🔧'
                     $entry.Name = $app.Name
@@ -193,7 +171,7 @@ class ApplicationRegistry
                     $entry.ActionKeys = $actionKeys
 
                     $entry.UserData = @{
-                        ScriptBlock = $callback
+                        ScriptBlock  = $callback
                         ArgumentList = $app.Source
                     }
 
@@ -205,10 +183,8 @@ class ApplicationRegistry
         } -ArgumentList $categories, $actionKeys, $callback, $this.sync
     }
 
-    [bool] UpdateEntries()
-    {
-        if ($this.registerEntryJob)
-        {
+    [bool] UpdateEntries() {
+        if ($this.registerEntryJob) {
             Receive-Job $this.registerEntryJob -Wait | Out-Null
             $this.registerEntryJob | Remove-Job
             $this.registerEntryJob = $null
