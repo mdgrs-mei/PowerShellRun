2799677241843e14bbf10654339c82109eef8421
Andrew Pla <AndrewPla@Outlook.com>
2024-02-28 23:45:00 -0500

    updated to -contains

--- module/PowerShellRun/Private/FileSystemRegistry.ps1
+++ module/PowerShellRun/Private/FileSystemRegistry.ps1
@@ -1,5 +1,4 @@
-class FileSystemRegistry
-{
+class FileSystemRegistry {
     $favoritesEntries = [System.Collections.Generic.List[PowerShellRun.SelectorEntry]]::new()
     $fileManagerEntry = [System.Collections.Generic.List[PowerShellRun.SelectorEntry]]::new()
 
@@ -10,22 +9,21 @@ class FileSystemRegistry
     [ScriptBlock]$defaultEditorScript
     $fileManagerArguments
 
-    FileSystemRegistry()
-    {
+    FileSystemRegistry() {
         $this.defaultEditorScript = {
             param ($path)
             Invoke-Item $path
         }
 
         $this.fileManagerArguments = @{
-            This = $this
-            FolderActionKeys = @(
+            This                = $this
+            FolderActionKeys    = @(
                 [PowerShellRun.ActionKey]::new($script:globalStore.firstActionKey, 'Go inside')
                 [PowerShellRun.ActionKey]::new($script:globalStore.secondActionKey, 'Set-Location')
                 [PowerShellRun.ActionKey]::new($script:globalStore.thirdActionKey, 'Open with default app')
                 [PowerShellRun.ActionKey]::new($script:globalStore.copyActionKey, 'Copy path to Clipboard')
             )
-            FileActionKeys = @(
+            FileActionKeys      = @(
                 [PowerShellRun.ActionKey]::new($script:globalStore.firstActionKey, 'Open with default app')
                 [PowerShellRun.ActionKey]::new($script:globalStore.secondActionKey, 'Edit with default editor')
                 [PowerShellRun.ActionKey]::new($script:globalStore.thirdActionKey, 'Open containing folder')
@@ -35,70 +33,59 @@ class FileSystemRegistry
                 param ($path)
                 $childItems = Get-ChildItem $path
                 $childItems | ForEach-Object {
-                    if ($_.PSIsContainer)
-                    {
+                    if ($_.PSIsContainer) {
                         $icon = '📁'
                     }
-                    else
-                    {
-                        $icon ='📄'
+                    else {
+                        $icon = '📄'
                     }
                     "{0} {1}" -f $icon, $_.Name
                 }
             }
-            PreviewScriptFile = {
+            PreviewScriptFile   = {
                 param ($path)
                 Get-Item $path | Out-String
             }
         }
     }
 
-    [System.Collections.Generic.List[PowerShellRun.SelectorEntry]] GetEntries()
-    {
+    [System.Collections.Generic.List[PowerShellRun.SelectorEntry]] GetEntries() {
         $entries = [System.Collections.Generic.List[PowerShellRun.SelectorEntry]]::new()
-        if ($this.isFavoritesEnabled)
-        {
+        if ($this.isFavoritesEnabled) {
             $entries.AddRange($this.favoritesEntries)
         }
-        if ($this.isFileManagerEnabled)
-        {
+        if ($this.isFileManagerEnabled) {
             $entries.AddRange($this.fileManagerEntry)
         }
         return $entries
     }
 
-    [void] EnableEntries([String[]]$categories)
-    {
+    [void] EnableEntries([String[]]$categories) {
         $this.isEntryUpdated = $true
 
-        $this.isFavoritesEnabled = $categories.Contains('Favorite')
+        $this.isFavoritesEnabled = $categories -Contains 'Favorite'
 
-        $this.isFileManagerEnabled = $categories.Contains('Utility')
+        $this.isFileManagerEnabled = $categories -Contains 'Utility'
         $this.fileManagerEntry.Clear()
-        if ($this.isFileManagerEnabled)
-        {
+        if ($this.isFileManagerEnabled) {
             $this.RegisterFileManagerEntry()
         }
     }
 
-    [void] SetDefaultEditorScript([ScriptBlock]$scriptBlock)
-    {
+    [void] SetDefaultEditorScript([ScriptBlock]$scriptBlock) {
         $this.defaultEditorScript = $scriptBlock
     }
 
-    [void] RegisterFileManagerEntry()
-    {
+    [void] RegisterFileManagerEntry() {
         $callback = {
             $result = $args[0].Result
             $arguments = $args[0].ArgumentList
             $rootDir = (Get-Location).Path
 
-            if ($result.KeyCombination -eq $script:globalStore.firstActionKey)
-            {
+            if ($result.KeyCombination -eq $script:globalStore.firstActionKey) {
                 & $arguments.This.fileManagerLoop $rootDir $arguments
             }
-            elseif ($result.KeyCombination -eq $script:globalStore.copyActionKey)
-            {
+            elseif ($result.KeyCombination -eq $script:globalStore.copyActionKey) {
                 $rootDir | Set-Clipboard
             }
         }
@@ -113,54 +100,47 @@ class FileSystemRegistry
         )
 
         $entry.UserData = @{
-            ScriptBlock = $callback
+            ScriptBlock  = $callback
             ArgumentList = $this.fileManagerArguments
         }
 
         $this.fileManagerEntry.Add($entry)
     }
 
-    [void] AddFavoriteFolder($folderPath, $icon, $name, $description, $preview)
-    {
+    [void] AddFavoriteFolder($folderPath, $icon, $name, $description, $preview) {
         $callback = {
             $result = $args[0].Result
             $arguments, $path = $args[0].ArgumentList
 
-            if ($result.KeyCombination -eq $script:globalStore.firstActionKey)
-            {
+            if ($result.KeyCombination -eq $script:globalStore.firstActionKey) {
                 & $arguments.This.fileManagerLoop $path $arguments
             }
-            elseif ($result.KeyCombination -eq $script:globalStore.secondActionKey)
-            {
+            elseif ($result.KeyCombination -eq $script:globalStore.secondActionKey) {
                 Set-Location $path
             }
-            elseif ($result.KeyCombination -eq $script:globalStore.thirdActionKey)
-            {
+            elseif ($result.KeyCombination -eq $script:globalStore.thirdActionKey) {
                 Invoke-Item $path
             }
-            elseif ($result.KeyCombination -eq $script:globalStore.copyActionKey)
-            {
+            elseif ($result.KeyCombination -eq $script:globalStore.copyActionKey) {
                 $path | Set-Clipboard
             }
         }
 
         $entry = [PowerShellRun.SelectorEntry]::new()
-        $entry.Icon = if ($icon) {$icon} else {'📁'}
-        $entry.Name = if ($name) {$name} else {Split-Path $folderPath -Leaf}
-        $entry.Description = if ($description) {$description} else {$folderPath}
-        if ($preview)
-        {
+        $entry.Icon = if ($icon) { $icon } else { '📁' }
+        $entry.Name = if ($name) { $name } else { Split-Path $folderPath -Leaf }
+        $entry.Description = if ($description) { $description } else { $folderPath }
+        if ($preview) {
             $entry.Preview = $preview
         }
-        else
-        {
+        else {
             $entry.PreviewAsyncScript = $this.fileManagerArguments.PreviewScriptFolder
             $entry.PreviewAsyncScriptArgumentList = $folderPath
         }
         $entry.ActionKeys = $this.fileManagerArguments.FolderActionKeys
 
         $entry.UserData = @{
-            ScriptBlock = $callback
+            ScriptBlock  = $callback
             ArgumentList = $this.fileManagerArguments, $folderPath
         }
 
@@ -168,47 +148,40 @@ class FileSystemRegistry
         $this.isEntryUpdated = $true
     }
 
-    [void] AddFavoriteFile($filePath, $icon, $name, $description, $preview)
-    {
+    [void] AddFavoriteFile($filePath, $icon, $name, $description, $preview) {
         $callback = {
             $result = $args[0].Result
             $arguments, $path = $args[0].ArgumentList
 
-            if ($result.KeyCombination -eq $script:globalStore.firstActionKey)
-            {
+            if ($result.KeyCombination -eq $script:globalStore.firstActionKey) {
                 & $script:globalStore.invokeFile $path
             }
-            elseif ($result.KeyCombination -eq $script:globalStore.secondActionKey)
-            {
+            elseif ($result.KeyCombination -eq $script:globalStore.secondActionKey) {
                 $arguments.This.EditWithDefaultEditor($path)
             }
-            elseif ($result.KeyCombination -eq $script:globalStore.thirdActionKey)
-            {
+            elseif ($result.KeyCombination -eq $script:globalStore.thirdActionKey) {
                 $arguments.This.OpenContainingFolder($path)
             }
-            elseif ($result.KeyCombination -eq $script:globalStore.copyActionKey)
-            {
+            elseif ($result.KeyCombination -eq $script:globalStore.copyActionKey) {
                 $path | Set-Clipboard
             }
         }
 
         $entry = [PowerShellRun.SelectorEntry]::new()
-        $entry.Icon = if ($icon) {$icon} else {'📄'}
-        $entry.Name = if ($name) {$name} else {Split-Path $filePath -Leaf}
-        $entry.Description = if ($description) {$description} else {$filePath}
-        if ($preview)
-        {
+        $entry.Icon = if ($icon) { $icon } else { '📄' }
+        $entry.Name = if ($name) { $name } else { Split-Path $filePath -Leaf }
+        $entry.Description = if ($description) { $description } else { $filePath }
+        if ($preview) {
             $entry.Preview = $preview
         }
-        else
-        {
+        else {
             $entry.PreviewAsyncScript = $this.fileManagerArguments.PreviewScriptFile
             $entry.PreviewAsyncScriptArgumentList = $filePath
         }
         $entry.ActionKeys = $this.fileManagerArguments.FileActionKeys
 
         $entry.UserData = @{
-            ScriptBlock = $callback
+            ScriptBlock  = $callback
             ArgumentList = $this.fileManagerArguments, $filePath
         }
 
@@ -225,22 +198,19 @@ class FileSystemRegistry
         $depth = 0
         $dir = $rootDir
         $parentDir = $null
-        while ($true)
-        {
+        while ($true) {
             $option.Prompt = "$dir> "
 
             $result = Get-ChildItem -Path $dir | ForEach-Object {
                 $entry = [PowerShellRun.SelectorEntry]::new()
                 $entry.UserData = $_
                 $entry.Name = $_.Name
-                if ($_.PSIsContainer)
-                {
+                if ($_.PSIsContainer) {
                     $entry.Icon = '📁'
                     $entry.PreviewAsyncScript = $arguments.PreviewScriptFolder
                     $entry.ActionKeys = $arguments.FolderActionKeys
                 }
-                else
-                {
+                else {
                     $entry.Icon = '📄'
                     $entry.PreviewAsyncScript = $arguments.PreviewScriptFile
                     $entry.ActionKeys = $arguments.FileActionKeys
@@ -249,15 +219,12 @@ class FileSystemRegistry
                 $entry
             } | Invoke-PSRunSelectorCustom -Option $option
 
-            if ($result.KeyCombination -eq 'Backspace')
-            {
-                if ($depth -eq 0)
-                {
+            if ($result.KeyCombination -eq 'Backspace') {
+                if ($depth -eq 0) {
                     Restore-PSRunFunctionParentSelector
                     break
                 }
-                else
-                {
+                else {
                     $depth--
                     $dir = $parentDir
                     $parentDir = ([System.IO.Directory]::GetParent($dir)).FullName
@@ -265,82 +232,65 @@ class FileSystemRegistry
                 }
             }
 
-            if (-not $result.FocusedEntry)
-            {
+            if (-not $result.FocusedEntry) {
                 break
             }
 
             $item = $result.FocusedEntry.UserData
-            if ($result.KeyCombination -eq $script:globalStore.firstActionKey)
-            {
-                if ($item.PSIsContainer)
-                {
+            if ($result.KeyCombination -eq $script:globalStore.firstActionKey) {
+                if ($item.PSIsContainer) {
                     $depth++
                     $parentDir = $dir
                     $dir = $item.FullName
                 }
-                else
-                {
+                else {
                     & $script:globalStore.invokeFile $item.FullName
                     break
                 }
             }
-            elseif ($result.KeyCombination -eq $script:globalStore.secondActionKey)
-            {
-                if ($item.PSIsContainer)
-                {
+            elseif ($result.KeyCombination -eq $script:globalStore.secondActionKey) {
+                if ($item.PSIsContainer) {
                     Set-Location $item.FullName
                 }
-                else
-                {
+                else {
                     $arguments.This.EditWithDefaultEditor($item.FullName)
                 }
                 break
             }
-            elseif ($result.KeyCombination -eq $script:globalStore.thirdActionKey)
-            {
-                if ($item.PSIsContainer)
-                {
+            elseif ($result.KeyCombination -eq $script:globalStore.thirdActionKey) {
+                if ($item.PSIsContainer) {
                     Invoke-Item $item.FullName
                 }
-                else
-                {
+                else {
                     $arguments.This.OpenContainingFolder($item.FullName)
                 }
                 break
             }
-            elseif ($result.KeyCombination -eq $script:globalStore.copyActionKey)
-            {
+            elseif ($result.KeyCombination -eq $script:globalStore.copyActionKey) {
                 $item.FullName | Set-Clipboard
                 break
             }
-            else
-            {
+            else {
                 break
             }
         }
     }
 
-    [void] OpenContainingFolder($path)
-    {
-        if ($script:isWindows)
-        {
+    [void] OpenContainingFolder($path) {
+        if ($script:isWindows) {
             & explorer.exe (('/select,{0}' -f $path).Split())
         }
-        else
-        {
+        else {
             $parentDir = ([System.IO.Directory]::GetParent($path)).FullName
             Invoke-Item $parentDir
         }
     }
 
-    [void] EditWithDefaultEditor($path)
-    {
+    [void] EditWithDefaultEditor($path) {
         $this.defaultEditorScript.Invoke($path)
     }
 
-    [bool] UpdateEntries()
-    {
+    [bool] UpdateEntries() {
         $updated = $this.isEntryUpdated
         $this.isEntryUpdated = $false
         return $updated
