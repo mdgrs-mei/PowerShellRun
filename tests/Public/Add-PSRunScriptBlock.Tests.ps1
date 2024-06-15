f52dc9fd3a4d163c3556b87a6de6e12aab82dc7f
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2024-06-15 10:10:32 +0900

    Add tests for script entries

---
+++ tests/Public/Add-PSRunScriptBlock.Tests.ps1
@@ -0,0 +1,17 @@
+﻿Describe 'Add-PSRunScriptBlock' {
+    BeforeEach {
+        Import-Module $PSScriptRoot/../../module/PowerShellRun -Force
+    }
+
+    It 'should add an entry' {
+        Add-PSRunScriptBlock -ScriptBlock { 'hello' } -Icon '😆' -Name 'Custom Name' -Description 'Custom Desc' -Preview 'Custom Preview'
+        InModuleScope 'PowerShellRun' {
+            $registry = $script:globalStore.GetRegistry('ScriptRegistry')
+            $registry.entries.Count | Should -Be 1
+        }
+    }
+
+    AfterEach {
+        Remove-Module PowerShellRun -Force
+    }
+}
---
+++ tests/Public/Add-PSRunScriptFile.Tests.ps1
@@ -0,0 +1,17 @@
+﻿Describe 'Add-PSRunScriptFile' {
+    BeforeEach {
+        Import-Module $PSScriptRoot/../../module/PowerShellRun -Force
+    }
+
+    It 'should add an entry' {
+        Add-PSRunScriptFile -Path 'D:/test.ps1' -Icon '😆' -Name 'Custom Name' -Description 'Custom Desc' -Preview 'Custom Preview'
+        InModuleScope 'PowerShellRun' {
+            $registry = $script:globalStore.GetRegistry('ScriptRegistry')
+            $registry.entries.Count | Should -Be 1
+        }
+    }
+
+    AfterEach {
+        Remove-Module PowerShellRun -Force
+    }
+}
