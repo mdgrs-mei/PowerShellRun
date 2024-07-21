6c1721a7884b4528249410992b05a8b7b692faab
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2024-07-21 21:46:23 +0900

    Add help

--- module/PowerShellRun/Public/Set-PSRunPSReadLineKeyHandler.ps1
+++ module/PowerShellRun/Public/Set-PSRunPSReadLineKeyHandler.ps1
@@ -1,3 +1,29 @@
+<#
+.SYNOPSIS
+Configures hotkeys for PowerShellRun features using PSReadLineKeyHandler.
+
+.DESCRIPTION
+Configures hotkeys for PowerShellRun features using PSReadLineKeyHandler.
+The chord can be a string or an array of strings. The format of the chord follows the spec of Set-PSReadLineKeyHandler.
+
+.PARAMETER InvokePsRunChord
+The key to call Invoke-PSRun.
+
+.PARAMETER PSReadLineHistoryChord
+The key to open PSReadLineHistory viewer.
+
+.INPUTS
+None.
+
+.OUTPUTS
+None.
+
+.EXAMPLE
+Set-PSRunPSReadLineKeyHandler -InvokePsRunChord 'Ctrl+Spacebar' -PSReadLineHistoryChord 'Ctrl+r'
+
+.LINK
+https://learn.microsoft.com/en-us/powershell/module/psreadline/set-psreadlinekeyhandler?view=powershell-7.4
+#>
 function Set-PSRunPSReadLineKeyHandler {
     [CmdletBinding()]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
--- module/PowerShellRun/Public/Start-PSRunFunctionRegistration.ps1
+++ module/PowerShellRun/Public/Start-PSRunFunctionRegistration.ps1
@@ -1,3 +1,26 @@
+<#
+.SYNOPSIS
+Starts function registration to PowerShellRun.
+
+.DESCRIPTION
+Starts function registration to PowerShellRun.
+The global functions defined after calling Start-PSRunFunctionRegistration are registered as function entries. You need to call Stop-PSRunFunctionRegistration at the end of function definitions.
+
+.INPUTS
+None.
+
+.OUTPUTS
+None.
+
+.EXAMPLE
+Start-PSRunFunctionRegistration
+
+function global:GitPullRebase() {
+    git pull --rebase
+}
+
+Stop-PSRunFunctionRegistration
+#>
 function Start-PSRunFunctionRegistration {
     [CmdletBinding()]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
