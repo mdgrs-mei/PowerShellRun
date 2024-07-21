3c6b165294271977523c91885443503156a1c454
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2024-07-21 15:46:26 +0900

    Add help

--- module/PowerShellRun/Public/Set-PSRunDefaultSelectorOption.ps1
+++ module/PowerShellRun/Public/Set-PSRunDefaultSelectorOption.ps1
@@ -1,3 +1,27 @@
+<#
+.SYNOPSIS
+Sets the default options for the PowerShellRun selectors.
+
+.DESCRIPTION
+Sets the default options for the PowerShellRun selectors. The option is used in all selector invocations such as Invoke-PSRun and Invoke-PSRunSelector.
+
+.PARAMETER Option
+Specifies PowerShellRun.SelectorOption that is used by default in all selector invocations.
+
+Use this command to see what properties are available:
+[PowerShellRun.SelectorOption]::new() | Get-Member -MemberType Properties
+
+.INPUTS
+None.
+
+.OUTPUTS
+None.
+
+.EXAMPLE
+$option = Get-PSRunDefaultSelectorOption
+$option.Theme.PreviewPosition = 'right'
+Set-PSRunDefaultSelectorOption -Option $option
+#>
 function Set-PSRunDefaultSelectorOption {
     [CmdletBinding()]
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseShouldProcessForStateChangingFunctions', '')]
