fb96768ea7e97ab5cc9844c191841a02fdc51967
mdgrs <81177095+mdgrs-mei@users.noreply.github.com>
2024-10-06 22:14:12 +0900

    Add ExitStatus

---
+++ src/PowerShellRun/Application/ExitStatus.cs
@@ -0,0 +1,18 @@
+﻿namespace PowerShellRun;
+
+// Stores the last exit status of the Selector or Prompt.
+// User applications can get the status through this class even if they can't directly get the selector/prompt result.
+// (e.g. the parent menu handling the restart key of the nested menus).
+public static class ExitStatus
+{
+    public static ExitType Type { get; internal set; } = ExitType.None;
+}
+
+public enum ExitType
+{
+    None,
+    Accept,
+    Quit,
+    QuitWithBackspaceOnEmptyQuery,
+    Restart,
+}
